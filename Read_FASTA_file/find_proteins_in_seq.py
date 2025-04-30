fb50783d705e3edd4f03405c24b237fdba32bf49
pySin <sinan_km@yahoo.com>
2025-04-30 20:41:02 +0100

    Proteins read

--- Read_FASTA_file/find_proteins_in_seq.py
+++ Read_FASTA_file/find_proteins_in_seq.py
@@ -8,4 +8,9 @@ DNA = SeqIO.read("gene2.fna", "fasta")
 # Get mRNA from DNA
 mRNA_sequence = DNA.seq.transcribe()
 
+# Get the amino acids from the RNA sequence
+amino_acids = mRNA_sequence.translate()
+
+print(f"Amino Acids: {amino_acids[:66]}")
+
 
