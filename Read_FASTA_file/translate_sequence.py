51e14f1b0d184323602763ac4a12170cdbe23054
pySin <sinan_km@yahoo.com>
2025-04-30 18:10:47 +0100

    Count all aminoacids

--- Read_FASTA_file/translate_sequence.py
+++ Read_FASTA_file/translate_sequence.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+from Bio.Seq import transcribe
 
 # DNA read
 DNA = SeqIO.read("gene2.fna", "fasta")
@@ -7,5 +8,12 @@ DNA_99 = DNA.seq[:99]
 # Transcribe the DNA to mRNA
 mRNA_sequence_99 = DNA_99.transcribe()
 
+# Full transcription
+mRNA_sequence = DNA.seq.transcribe()
+
 amino_acids = mRNA_sequence_99.translate()
+full_amino_acids = mRNA_sequence.translate()
+
 print(f"Amino Acids: {amino_acids}")
+print(f"All Amino Acids: {full_amino_acids}")
+print(f"Count all aminoacids: {len(full_amino_acids)}")
